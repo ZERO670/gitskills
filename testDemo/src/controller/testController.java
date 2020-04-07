4ebdc0c62d890e4afeb156f34747a1858a375e1e
ZERO670 <670167267@qq.com>
2020-04-08 00:05:30 +0800

    ThirdPush

--- testDemo/src/controller/testController.java
+++ testDemo/src/controller/testController.java
@@ -3,6 +3,6 @@ package controller;
 public class testController {
 
     public static void main(String[] args){
-
+        System.out.println("testController");
     }
 }
--- testDemo/src/controller/testDemo.java
+++ testDemo/src/controller/testDemo.java
@@ -3,6 +3,6 @@ package controller;
 public class testDemo {
 
     public static void main(String[] args){
-
+        System.out.println("testController");
     }
 }
